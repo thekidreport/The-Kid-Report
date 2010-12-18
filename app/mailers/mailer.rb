af660a1d43d908dd1ebe94882a95c95b2d4e2f92
Swards <swards@gmail.com>
2010-12-18 13:50:57 -0800

    Fixing mailers

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -9,49 +9,28 @@ class Mailer < ActionMailer::Base
     end
   end
   
-  
-  # The rest need to be cleaned up and added to cron jobs.
   def site_message (message)    
-    content_type "text/html"
-    recipients message.users.map(&:email)
-    from 'support@thekidreport.com'
-    @headers['Reply-to'] = message.user.email
-    subject message.subject
-
-    body :user => message.user, :site => message.site, :message => message.body.gsub(/$/,'<br>')
+    mail( :from => message.user.email, :to => message.recipients.map(&:email), :subject => message.subject ) do |format|
+      format.html { render 'site_message' }
+    end
   end
   
   def status_update (user_count, site_count)
-    recipients 'swards@gmail.com'
-    from 'support@thekidreport.com'
-    subject "Status update"
-    
-    body :user_count => user_count, :site_count => site_count
+    @user_count = user_count
+    @site_count = site_count
+    mail( :to => 'swards@gmail.com', :subject => "Kidreport status update" ) do |format|
+      format.html { render 'status_update' }
+    end
   end
 
   def site_update (user, site)
-    # Email header info MUST be added here
-    content_type "text/html"
-    recipients user.email
-    from 'support@thekidreport.com'
-    subject "#{site.name} update"
-
-    # Email body substitutions go here
-    @body["user"]          = user
-    @body["site"]          = site
+    @user = user
+    @site = site
+    mail( :to => user.email, :subject => "#{site.name} update" ) do |format|
+      format.html { render 'site_update' }
+    end
   end
   
-  def comment (comment)
-
-    content_type "text/html"
-    recipients 'support@thekidreport.com'
-    if (comment.user)
-      @headers['Reply-to'] = comment.user.email
-      from comment.user.email
-    end
-    subject comment.subject
-    body :comment => comment
-    content_type "text/html" 
-  end
+  # Send email on comment
   
 end
