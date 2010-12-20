0943d96c21a1da8fb470cca5f4e4fdb87be7de3c
Swards <swards@gmail.com>
2010-12-19 22:59:26 -0800

    Adding analytics.Updating about us.  Fixing bug in comment destroy:

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,7 +12,7 @@ class CommentsController < ApplicationController
   def destroy
     @page = @site.pages.find(params[:page_id])
     @comment = @page.comments.find(params[:id])
-    @comment.destroy if (current_user.can_edit?(@site) || comment.user.eql?(current_user))
+    @comment.destroy if (current_user.can_edit?(@site) || @comment.user.eql?(current_user))
     render :update do |page|
       page.replace_html 'comments', :partial => "comments/page_comments", :locals => { :page => @comment.page }
     end
