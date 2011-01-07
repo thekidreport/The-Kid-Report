f5ad2e73f4913204242dcdb3ab921e6dc2f923b6
Swards <swards@gmail.com>
2011-01-06 21:35:43 -0800

    Fixing bad email entries in memberships

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -9,6 +9,7 @@ class Membership < ActiveRecord::Base
     
     validates_presence_of :user
     validates_presence_of :role
+    validates_format_of :email, :with => /^[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/, :if => :email
     
     validates_uniqueness_of :user_id, :scope => :site_id, :message => 'is already a member'
     
