a1890eadc71c50401d19a179130313868d0057f1
Swards <swards@gmail.com>
2011-02-06 12:19:54 -0800

    Adding columns to user/admin

--- config/initializers/datetime.rb
+++ config/initializers/datetime.rb
@@ -1,3 +1,4 @@
 Time::DATE_FORMATS[:date] = "%a %b %d, %Y"
+Time::DATE_FORMATS[:tiny_date] = "%m/%d/%y"
 Time::DATE_FORMATS[:time] = "%I:%M%p"
 Time::DATE_FORMATS[:pretty] = lambda { |time| time.strftime("%a %b %d, %Y %l:%M%p") }
