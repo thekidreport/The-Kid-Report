a103e17af1682fb636bf15deaeca20ed5c743a22
Swards <swards@gmail.com>
2011-04-03 08:55:19 -0700

    More cleanup on events

--- config/initializers/string_extensions.rb
+++ config/initializers/string_extensions.rb
@@ -1,25 +1,29 @@
 class String
   
-  def ellipsis length
+  def truncate length
     if length > 0
       self.strip!
       if self.length > length
         out_array = []
-        words = self.split(/ /)
+        words = self.split(' ')
         for word in words
           if ((out_array + [word]).join(' ').length) < length
-            out_array += [word]
+            out_array << word
           else
             break
           end
         end
         string_to_return = out_array.join(' ')
+        
+        # in case truncate is too short
         if string_to_return.length == 0
           string_to_return = self[0..length]
         end
       
         # remove any trailing spaces.
         string_to_return.strip! 
+        
+        # special last characters
         if string_to_return[-1,1] == '.'
           string_to_return += '..'
         elsif string_to_return[-1,1] == ','
